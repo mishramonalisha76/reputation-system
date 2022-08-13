cd7901c3cefe557fb816bf42a50cbf5f6d571214
Monalisha Mishra <monalisha.mishra@factset.com>
2022-08-13 14:12:22 +0530

    added contract call

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,16 +1,57 @@
-import React, { useState } from "react";
-import { useNavigate  } from "react-router-dom";
+import React, { useState,useContext} from "react";
+import { useNavigate } from "react-router-dom";
+import {WalletAddressContext} from "../../context";
 import "./login.css";
 
 export default function Login() {
-  const navigate  = useNavigate();
+  const navigate = useNavigate();
+  const { walletAddress, setWalletAddress } = useContext(WalletAddressContext);
   const [githubId, setGithubId] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found!");
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            "Connected with Public Key:",
+            response.publicKey.toString()
+          );
+
+          /*
+           * Set the user's publicKey in state to be used later!
+           */
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert("Solana object not found! Get a Phantom Wallet 👻");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with Public Key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
 
   const handleSubmit = () => {
     if (githubId === "") {
       alert("Enter github id");
+    } 
+    if (!walletAddress) {
+      alert("Please connect you wallet");
     } else {
-      navigate(`/profile/${githubId}/23144`);
+      navigate(`/profile/${githubId}/${walletAddress}`);
     }
   };
   return (
@@ -28,7 +69,16 @@ export default function Login() {
           value={githubId}
           onChange={(e) => setGithubId(e.target.value)}
         />
-        <button className="login-card-item login-button">Connect Wallet</button>
+        {walletAddress ? (
+          <p className="login-card-item">{walletAddress}</p>
+        ) : (
+          <button
+            className="login-card-item login-button"
+            onClick={connectWallet}
+          >
+            Connect Wallet
+          </button>
+        )}
         <button
           className="login-card-item login-button"
           onClick={() => handleSubmit()}
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,22 +1,148 @@
-import React,{useState,useEffect} from "react";
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 import axios from "axios";
+import { WalletAddressContext } from "../../context";
+import idl from "../../idl.json";
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { Program, AnchorProvider, web3 } from '@project-serum/anchor';
 import "./profile.css";
 
 export default function Profile() {
   const { githubId } = useParams();
-  const [githubData,setGithubData] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { walletAddress, setWalletAddress } = useContext(WalletAddressContext);
+  const [githubData, setGithubData] = useState([]);
+  const [recentWork, setRecentWork] = useState({});
+  const [githubEventData, setGithubEventData] = useState([]);
+  const [totalCommits, setTotalCommits] = useState(0);
+  
+// SystemProgram is a reference to the Solana runtime!
+const { SystemProgram, Keypair } = web3;
+
+// Create a keypair for the account that will hold the GIF data.
+let baseAccount = Keypair.generate();
+
+// Get our program's id from the IDL file.
+const programID = new PublicKey(idl.metadata.address);
+
+// Set our network to devnet.
+const network = clusterApiUrl('devnet');
+
+// Controls how we want to acknowledge when a transaction is "done".
+const opts = {
+  preflightCommitment: "processed"
+}
+
   useEffect(() => {
     axios
-      .get(`https://api.github.com/users/${githubId}/events`)
-      .then((response) => {
-        console.log(response);
-        setGithubData(response.data)
-      })
-      .catch((err) => {
-      
-      });
+      .all([
+        axios.get(`https://api.github.com/users/${githubId}/events`),
+        axios.get(`https://api.github.com/users/${githubId}`),
+      ])
+
+      .then(
+        axios.spread((response1, response2) => {
+          console.log(response2);
+          setGithubData(response2.data);
+          setGithubEventData(response1.data);
+          setRecentWork(getRecentWork(response1.data));
+        })
+      )
+      .catch((err) => {});
+      if (walletAddress) {
+        console.log('Fetching Account list...');
+        createProfileAccount();
+      }
   }, []);
+
+  const createProfileAccount = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      console.log("ping")
+      await program.rpc.startStuffOff({
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount]
+      });
+      console.log("Created a new BaseAccount w/ address:", baseAccount.publicKey.toString())
+      await getAccountDetails();
+  
+    } catch(error) {
+      console.log("Error creating BaseAccount account:", error)
+    }
+  }
+
+  const getAccountDetails = async() => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+      
+      console.log("Got the account", account)
+  
+    } catch (error) {
+      console.log("Error in getAccountDetails: ", error)
+
+    }
+  }
+
+  const addProfile = async() => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      console.log(program)
+      const account = await program.methods.addProfile(githubData.url,'');
+      
+      console.log("Added profile", account)
+  
+    } catch (error) {
+      console.log("Error in addProfile: ", error)
+
+    }
+  }
+
+  
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new AnchorProvider(
+      connection, window.solana, opts.preflightCommitment,
+    );
+    return provider;
+  }
+
+  const getRecentWork = (events) => {
+    let lastCommit;
+
+    events.some((event) => {
+      return (
+        event.type === "PushEvent" &&
+        event.payload.commits.reverse().some((commit) => {
+          // if (commit.author.email === EMAIL) {
+          lastCommit = {
+            repo: event.repo.name,
+            sha: commit.sha,
+            time: new Date(event.createdAt),
+            message: commit.message,
+            url: commit.url,
+          };
+
+          return true;
+          // }
+        })
+      );
+    });
+    return lastCommit;
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(
+      `http://localhost:3000/profile/${githubId}/${walletAddress}/?linkShare=share`
+    );
+  };
   return (
     <div className="profile-card">
       <div className="profile-card-right">
@@ -29,36 +155,57 @@ export default function Profile() {
         <hr className="profile-hr" />
         <div className="profile-column">
           <div className="profile-column-left">
-            <h3>Off-Chain</h3>
-            <div>
-                <p>GithubID</p>
-            </div>
-            <div>
-                <p>Recent Work</p>
-            </div>
-            <div>
-                <p>Repositories</p>
-            </div>
-            <div>
-                <p>Total Commits</p>
-            </div>
+            <h2>Off-Chain</h2>
+            {githubData ? (
+              <>
+                <div>
+                  <a herf={githubData.url}>{githubId}</a>
+                  <p>GithubID</p>
+                </div>
+                <div>
+                  <a herf={recentWork.repo}>
+                    {recentWork.repo && recentWork.repo.split("/").pop()}
+                  </a>
+                  <p>Recent Work</p>
+                </div>
+                <div>
+                  <a herf={githubData.repos_url}>{githubData.public_repos}</a>
+                  <p>Repositories</p>
+                </div>
+                {/* <div>
+              <p>{totalCommits}</p>
+              <p>Total Commits</p>
+            </div> */}
+              </>
+            ) : (
+              <p>No Off-chain data</p>
+            )}
           </div>
           <div className="profile-column-right">
-            <h3>On-Chain</h3>
+            <h2>On-Chain</h2>
             <div>
-                <p>NFTs</p>
+              <p>NFTs</p>
             </div>
             <div>
-                <p>Courses</p>
+              <p>Courses</p>
             </div>
             <div>
-                <p>Balance</p>
+              <p>Balance</p>
             </div>
           </div>
         </div>
-        <button className="profile-card-item profile-button">
-          Store On-Chain
+        <button
+          className="profile-card-item profile-button"
+          onClick={() => copyToClipboard()}
+        >
+          Get sharable link
         </button>
+        {!searchParams.get("linkShare") && (
+          <button className="profile-card-item profile-button"
+          onClick={() => addProfile()}>
+            Store On-Chain
+          </button>
+        )}
       </div>
     </div>
   );
